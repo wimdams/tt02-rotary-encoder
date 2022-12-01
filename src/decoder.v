3b010b2b50d6a1881824ab4cee17df77bda674c5
Wim Dams <24987234+wimdams@users.noreply.github.com>
2022-12-02 00:15:13 +0100

    Changed the 7 segment codes for 6 and 9 + picture added to info.yaml

--- src/decoder.v
+++ src/decoder.v
@@ -25,10 +25,10 @@ module seg7 (
             3:  segments = 7'b1001111;
             4:  segments = 7'b1100110;
             5:  segments = 7'b1101101;
-            6:  segments = 7'b1111100;
+            6:  segments = 7'b1111101;
             7:  segments = 7'b0000111;
             8:  segments = 7'b1111111;
-            9:  segments = 7'b1100111;
+            9:  segments = 7'b1101111;
 
 //            //only for simulation
 //            0:  segments = 7'b0000000;
