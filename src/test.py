27364bf91768e460f7aa1559050cd74ab17c8f2b
Wim Dams <24987234+wimdams@users.noreply.github.com>
2022-12-01 23:52:34 +0100

    Created a python test bench for the rotary encoder (count up/down)

--- src/test.py
+++ src/test.py
@@ -3,7 +3,8 @@ from cocotb.clock import Clock
 from cocotb.triggers import RisingEdge, FallingEdge, Timer, ClockCycles
 
 
-segments = [ 63, 6, 91, 79, 102, 109, 124, 7, 127, 103 ]
+segmentsforward = [ 63, 6, 91, 790, 102, 109, 124, 7, 127, 103 ]
+segmentsbackward = [ 63, 103, 1270, 7, 124, 109, 102, 79, 91, 6 ]
 
 @cocotb.test()
 async def rotary_encoder(dut):
@@ -13,13 +14,26 @@ async def rotary_encoder(dut):
 
     dut._log.info("reset")
     dut.rst.value = 1
-    await ClockCycles(dut.clk, 10)
     dut.A.value = 0
     dut.B.value = 0
+    await ClockCycles(dut.clk, 10)
     dut.rst.value = 0
 
-    dut._log.info("check all segments")
+    dut._log.info("Rotating forward")
     for i in range(10):
+        dut.A.value = 0
         dut._log.info("check segment {}".format(i))
+        assert int(dut.segments.value) == segmentsforward[i]
         await ClockCycles(dut.clk, 100)
-        #assert int(dut.segments.value) == segments[i]
+        dut.A.value = 1
+        await ClockCycles(dut.clk, 100)
+
+    dut._log.info("Rotating backward")
+    dut.B.value = 1
+    for i in range(10):
+        dut.A.value = 0
+        dut._log.info("check segment {}".format(i))
+        assert int(dut.segments.value) == segmentsbackward[i]
+        await ClockCycles(dut.clk, 100)
+        dut.A.value = 1
+        await ClockCycles(dut.clk, 100)
